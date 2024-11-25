32c876e4c037375788266461bce31a03edd0f891
Pedro Leite <pedroleite1494@gmail.com>
2024-11-25 10:33:07 +0000

    comment edited

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <string.h> // Biblioteca para strlen
 int main() {
+
     char nome [50];
     int numero;
     char palavra[50];
     int dobroNumero;
     int numChar;
+
+    int dobleInt(int numero){
+        dobroNumero= numero*2;
+    }
 
     // Entrada nome
     printf("Digite o seu nome: ");
@@ -14,7 +19,10 @@ int main() {
     // Entrada do numero
     printf("Digite um numero: ");
     scanf("%d", &numero);
-    dobroNumero=numero*2;
+
+    //dobroNumero=numero*2;
+    dobleInt(numero);
+
     // Entrada da palavra
     printf("Digite uma palavra: ");
     scanf("%s", palavra);
@@ -24,6 +32,8 @@ int main() {
     printf("Ola, %s! O numero que escolheste e %d.\n", nome, numero);
     printf("Ola, %s! O dobro do numero que escolheste e %d.\n", nome, dobroNumero);
     printf("A palavra e: %s.\n", palavra);
-    printf("O numero mais o comprimento da palavra e: %d.\n", numero + strlen(palavra)); return 0;
+    printf("O numero mais o comprimento da palavra e: %d.\n", numero + strlen(palavra));
+
+    return 0;
 }
 
